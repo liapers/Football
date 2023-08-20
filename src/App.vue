8d816c3f7c457a8b604261041f5098a494e6c327
Liana <lianam881@gmail.com>
2023-08-21 00:57:49 +0500

    #1 шапка

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import Header from './components/Header.vue';
 </script>
 
 <template>
-  <RouterView />
+  <div class="container">
+    <Header/>
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav class="flex items-center bg-white h-[70px] rounded-2xl mb-4">
+        <div class="pl-4">
+            <ul class="m-0 flex">
+                <li class="mr-4">
+                    Матчи
+                </li>
+                <li class="mr-4">
+                    Команды
+                </li>
+            </ul>
+
+        </div>
+    </nav>
+</template>
