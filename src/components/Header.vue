5e1c883b90fae2b2bcf801378fa23d7cac1d8e7b
Liana <lianam881@gmail.com>
2023-08-22 14:23:04 +0500

    #5 красивые ссылки

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,40 @@
 <template>
-    <nav class="flex items-center bg-white h-[70px] rounded-2xl mb-4">
-        <div class="pl-4">
-            <ul class="m-0 flex">
-                <li class="mr-4">
+  <nav class="flex items-center bg-white h-[70px] rounded-2xl mb-4">
+    <div class="pl-4">
+      <ul
+        class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+      >
+        <li>
+            <router-link
+                :to="{name:'leagues'}"
+                v-slot="{ isActive, href}"
+            >
+                <a
+                    :href="href"
+                    :class="isActive ?
+                    'block py-2 pl-3 pr-4 text-white bg-cyan-500 rounded md:bg-transparent md:text-cyan-500 md:p-0 dark:text-white md:dark:text-blue-500' :
+                    'block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-cyan-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'"
+                >
+                    Лиги
+                </a>
+            </router-link>
+        </li>
+        <li>
+            <router-link
+                :to="{name: 'leagues'}"
+                v-slot="{ isActive, href}"
+            >
+                <a
+                    :href="href"
+                    :class="isActive ?
+                    'block py-2 pl-3 pr-4 text-white bg-cyan-500 rounded md:bg-transparent md:text-cyan-500 md:p-0 dark:text-white md:dark:text-blue-500' :
+                    'block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-cyan-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'"
+                >
                     Матчи
-                </li>
-                <li class="mr-4">
-                    Команды
-                </li>
-            </ul>
-
-        </div>
-    </nav>
+                </a>
+            </router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
