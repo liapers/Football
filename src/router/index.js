71ac5d9cd5515666a9db3a08fc4501d194032c02
Liana <lianam881@gmail.com>
2023-08-25 07:39:35 +0500

    #10 затравка матча

--- src/router/index.js
+++ src/router/index.js
@@ -6,13 +6,34 @@ const router = createRouter({
     {
       path: '/',
       name: 'leagues',
-      component:  () => import('../views/Leagues.vue')
+      component:  () => import('@/views/Leagues.vue'),
+      meta: { title: 'Лиги' }
+    },
+    {
+      path: '/leagues/:id(\\d+)+/',
+      name: 'league',
+      component: () => import('@/views/Leagues.vue'),
+      meta: { title: 'Лига' }
     },
     {
       path: '/teams/',
       name: 'teams',
-      component:  () => import('../views/Teams.vue')
+      component:  () => import('@/views/Teams.vue'),
+      meta: { title: 'Команды' }
     },
+    {
+      path: '/match/:id(\\d+)+/',
+      name: 'match',
+      props: true,
+      component: () => import('@/views/Match.vue'),
+      meta: { title: 'Матч' }
+    },
+    {
+      path: '/team/:id(\\d+)+/',
+      name: 'team',
+      props: true,
+      component: () => import('@/views/Teams.vue')
+    }
   ]
 })
 
